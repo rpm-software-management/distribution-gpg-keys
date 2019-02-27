48ca37d18d8dbc7cad0f0de81e65f32610a0fb39
Jan Buchmaier <janbuchmaier@seznam.cz>
2019-02-27 09:57:07 +0100

    Converted into API 3, added old keys removing.

--- copr-gpg-download.py
+++ copr-gpg-download.py
@@ -1,51 +1,62 @@
 #!/usr/bin/python3
 
-from copr import create_client2_from_params
-import requests
-import argparse
-from argparse import RawTextHelpFormatter
-import os.path
+from os import path, remove
+from argparse import RawTextHelpFormatter, ArgumentParser
+from requests import get
+from copr.v3 import Client
+from copr.v3.pagination import next_page
 
-def get_gpg(project):
-    url = be_url_tmpl.format(**{'username': project.owner, 'projectname': project.name})
-    r = requests.get(url)
-    return r.text
 
-def gpg_out(isolate_file, project):
+def get_gpg(url):
+
+    response = get(url)
+    return response.text
+
+
+def gpg_out(isolate_file, file_name, response):
     if isolate_file:
-        file_name = "copr-{0}-{1}.gpg".format(project.owner, project.name)
-        if not os.path.isfile(file_name):
-            f = open(file_name, 'w')
-            f.write(get_gpg(project))
-            f.close()
+        if not path.isfile(file_name):
+            with open(file_name, "w") as file:
+                file.write(response)
+
             print("Saved {0}".format(file_name))
         else:
             print("Skipping {0} - already downloaded.".format(file_name))
     elif output_file:
-        output_file.write(gpg)
+        output_file.write(response)
     else:
-        print(gpg)
+        print(response)
+
 
 def main():
-    kwargs = {}
-
-    if args.user:
-        kwargs['owner'] = args.user
-    if args.project:
-        kwargs['name'] = args.project
-
-    _offset = 0
-    _limit = 100
-
-    while True:
-        projects = cli.projects.get_list(offset=_offset, limit=_limit, **kwargs)
+    projects = cli.project_proxy.get_list(ownername=None, pagination={"limit": 100})
+    while projects:
         if not projects:
             break
-        for project in projects:
-            gpg_out(args.isolate_files, project)
-        _offset += _limit
 
-parser = argparse.ArgumentParser(description='Download GPG keys for COPR projects.', formatter_class=RawTextHelpFormatter)
+        for project in projects:
+            file_name = args.path + "copr-{0}-{1}.gpg".format(project.ownername, project.name)
+            url = be_url_tmpl.format(**{'username': project.ownername,
+                                        'project_name': project.name})
+            response = get_gpg(url)
+            if "404 Not Found" in response:
+                not_found(file_name)
+            else:
+                gpg_out(args.isolate_files, file_name, response)
+
+        projects = next_page(projects)
+
+
+def not_found(file_name):
+    if path.isfile(file_name):
+        print("Deleting {0} - project key doesn't exist.".format(file_name))
+        remove(file_name)
+    else:
+        print("Skipping {} - already deleted.".format(file_name))
+
+
+parser = ArgumentParser(description='Download GPG keys for COPR projects.',
+                        formatter_class=RawTextHelpFormatter)
 parser.add_argument('-f', '--file', action='store',
                     help='store keys to a file instead of printing to stdout')
 parser.add_argument('--feurl', action='store', default='http://copr.fedorainfracloud.org/',
@@ -58,17 +69,22 @@ parser.add_argument('--project', action='store',
                     help='only download gpg keys for projects of this name')
 parser.add_argument('--isolate-files', action='store_true',
                     help='Each GPG file is stored in separate file.')
+parser.add_argument('--path', action='store', default='./',
+                    help='path to folder where keys are stored')
 
 args = parser.parse_args()
 
-be_url_tmpl = args.beurl+'/results/{username}/{projectname}/pubkey.gpg'
+be_url_tmpl = args.beurl+'/results/{username}/{project_name}/pubkey.gpg'
 
-cli = create_client2_from_params(root_url=args.feurl)
+cli = Client.create_from_config_file()
+cli.config["copr_url"] = args.feurl
+
 if args.file:
     output_file = open(args.file, 'w')
 else:
     output_file = None
+
 try:
     main()
-except KeyboardInterrupt as e:
+except KeyboardInterrupt as error:
     pass
